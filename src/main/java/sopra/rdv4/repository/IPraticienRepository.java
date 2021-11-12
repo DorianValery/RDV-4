547fdb2f9c3551ed7e9dedc7848e2846bf2513a9
Dorian Valery <d.valery12@gmail.com>
2021-11-12 14:52:57 +0100

    modif IPraticien

--- src/main/java/sopra/rdv4/repository/IPraticienRepository.java
+++ src/main/java/sopra/rdv4/repository/IPraticienRepository.java
@@ -1,9 +1,20 @@
 package sopra.rdv4.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import sopra.rdv4.model.Praticien;
 
 public interface IPraticienRepository extends JpaRepository<Praticien, Long>  {
 
+	List <Praticien> findByNom (String nom);
+	
+	@Query("from Praticien p join p.specialites s where s.nom=:nom")
+	List <Praticien> findBySpecialiteNom (@Param("nom") String nom);
+	
+	@Query("from Praticien p join p.lieux l where l.ville like '%:ville%'")
+	List <Praticien> findByVille (@Param("ville") String ville);
 }
