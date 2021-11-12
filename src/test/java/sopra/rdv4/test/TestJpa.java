b65bcd273a2661095102c0cfb845723f6a92444d
Vincent <vincent.dehouve@supbioteh.fr>
2021-11-12 14:04:40 +0100

    Auto stash before merge of "master" and "origin/master"

--- src/test/java/sopra/rdv4/test/TestJpa.java
+++ src/test/java/sopra/rdv4/test/TestJpa.java
@@ -1,9 +1,35 @@
+<<<<<<< Updated upstream:src/test/java/sopra/rdv4/test/TestJpa.java
 package sopra.rdv4.test;
+=======
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import sopra.rdv4.config.ApplicationConfig;
+import sopra.rdv4.repository.IConsultationRepository;
+import sopra.rdv4.repository.IPatientRepository;
+import sopra.rdv4.repository.IPraticienRepository;
+
+>>>>>>> Stashed changes:src/test/java/TestJpa.java
 
 public class TestJpa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		AnnotationConfigApplicationContext spring = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+
+		IConsultationRepository consultationRepo = spring.getBean(IConsultationRepository.class);
+		ICreneauRepository creneauRepo = spring.getBean(ICreneauRepository.class);
+		ILieuRepository lieuRepo = spring.getBean(ILieuRepository.class);
+		IMotifRepository motifRepo = spring.getBean(IMotifRepository.class);
+		IPatientRepository patientRepo = spring.getBean(IPatientRepository.class);
+		IPraticienRepository praticienRepo = spring.getBean(IPraticienRepository.class);
+		ISpecialiteRepository specialiteRepo = spring.getBean(ISpecialiteRepository.class);
+		IUtilisateurRepository utilisateurRepo = spring.getBean(IUtilisateurRepository.class);
+
+		
+		//Evaluation evalLea = new Evaluation(15, 13, "super délégué");
+
+		//evalLea = evaluationRepo.save(evalLea);
+		
+		spring.close();
 
 	}
 
