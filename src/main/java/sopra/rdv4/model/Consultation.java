c506d7dc34611e9139956feaa0a3d191daa38e30
Fabiola <fabiola.ouedraogo@gmail.com>
2021-11-12 13:41:27 +0100

    Ajouts attributs+getters/setters

--- src/main/java/sopra/rdv4/model/Consultation.java
+++ src/main/java/sopra/rdv4/model/Consultation.java
@@ -1,9 +1,36 @@
 package sopra.rdv4.model;
 
 public class Consultation {
+	private Long id;
+	private String instructions;
 
-	public Consultation() {
-		// TODO Auto-generated constructor stub
+	public Consultation() {}
+
+	public Consultation(Long id, String instructions) {
+		super();
+		this.id = id;
+		this.instructions = instructions;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getInstructions() {
+		return instructions;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setInstructions(String instructions) {
+		this.instructions = instructions;
+	}
+
+	@Override
+	public String toString() {
+		return "Consultation [id=" + id + ", instructions=" + instructions + "]";
 	}
 
 }
